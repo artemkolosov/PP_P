c48a4653832f03bdf258482a7af0bb3a398e5c46
artemkolosov <artemkol@mail.ru>
2024-01-21 12:42:37 +0300

    Тест1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,5 +3,8 @@ package jm.task.core.jdbc;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+
+        // реализуйте алгоритм здесь
     }
 }
